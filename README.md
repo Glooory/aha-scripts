b2745bba72f656334c63ff907e27773366ea6d09
fcn_putao <tao.pu@fenrir-inc.com.cn>
2023-04-06 18:20:03 +0800

    Update README

---
+++ README.md
@@ -0,0 +1,17 @@
+### 一些自用的 Tampermonkey 脚本
+
+#### 微信读书自定义布局
+修改了一下微信读书的样式，主要目的是显示更多文本。
+
+本身的样式：
+![wead_original](./assets/weread_original.png)
+
+修改后的样式：
+![wead_modified](./assets/weread_modified.png)
+
+#### 自动清除知乎登录弹窗
+访问知乎没登录的话，每次都要弹登录弹窗，懒得手动点关闭了。
+
+![rm_zhihu_login_modal](./assets/rm_zhihu_login_modal.gif)
+
+
---
+++ scripts/custom-weread-style.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         微信读书自定义布局
+// @namespace    glooory
+// @version      0.1
+// @description  Make it much more.
+// @author       glooorypu@gmail.com
+// @match        https://weread.qq.com/web/*
+// @icon         https://rescdn.qqmail.com/node/wr/wrpage/style/images/independent/favicon/favicon_48h.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  function addGlobalStyle(css, id) {
+    let head, style;
+    if (id && document.getElementById(id)) return;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.id = id;
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+  }
+
+  const appContentCSS = "div.app_content { max-width: 100vw !important; width: min(1400px, 100vw) !important; }";
+  const topNavBarCSS = "div.readerTopBar { max-width: 100vw !important; width: min(1400px, 100vw) !important; height: auto; }";
+  const homeNavLogoCSS = "a.readerTopBar_home { width: 40px; height: 40px; }"
+  const floatBtnsCSS = "div.readerControls { left: auto; right: 16px; bottom: 24px; width: auto; margin-left: 0; }"
+  const nextChapterBtnCSS = "div.readerFooter button { position: fixed; right: 10px; bottom: 420px; width: auto; padding: 16px 8px; line-height: 1; height: auto; }"
+  const prevChapterBtnCSS = "div.readerContentHeader button { position: fixed; right: 10px; bottom: 492px; width: auto; padding: 16px 8px; line-height: 1; height: auto; border-radius: 12px;text-align: center;font-size: 16px;font-weight: 500; background-color: #f6f7f9;}";
+  const catalogModalCSS = ".wr_whiteTheme div.readerCatalog { right: 0; margin-left: auto; }";
+  const customCSS = appContentCSS + topNavBarCSS + homeNavLogoCSS + floatBtnsCSS + nextChapterBtnCSS + prevChapterBtnCSS + catalogModalCSS;
+
+  addGlobalStyle(customCSS, "custom_reader_layout");
+})();
